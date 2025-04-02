aa87d3d1b84945a9caacfc5f567a2630182939a0
Anuva13 <anuvadas@hotmail.com>
2025-04-02 14:50:09 -0400

    Initial commit

---
+++ MensuraPy/area.py
@@ -0,0 +1,27 @@
+import math
+from param_validator import *
+
+def bekar():
+    hello()
+    
+bekar()
+    
+"""
+def square(side):
+    # Returns the area of a square.
+    return side ** 2
+
+def rectangle(length,breadth):
+    # Returns the area of a rectangle.
+    return length * breadth
+
+def circle(radius):
+    # Returns the area of a circle.
+    return math.pi * radius ** 2
+
+def area_base_height(base, height):
+    # Returns the area of a triangle when base and height are given
+    return 0.5 * base * height
+
+def area_heros_formula(side1, side2, side3)
+"""
